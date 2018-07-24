853d31735fdfecca7fc91123a763443cc62b3fed
K H <khasegawa@users.noreply.github.com>
2018-07-24 15:14:33 +0900

    Add project files.

---
+++ BCC myself OutlookAddIn/ThisAddIn.cs
@@ -0,0 +1,48 @@
+﻿using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Threading;
+using System;
+
+namespace BCC_myself_OutlookAddIn {
+    public partial class ThisAddIn {
+        Outlook.MailItem myMail;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e) {
+            Application.ItemLoad += new Outlook.ApplicationEvents_11_ItemLoadEventHandler(Application_ItemLoad);
+        }
+
+        public void Application_ItemLoad(object Item) {
+            if (Item is Outlook.MailItem) {
+                myMail = Item as Outlook.MailItem;
+                (new Thread(() => {
+                    Thread.Sleep(100);
+                    try {
+                        Type dummy = Application.ActiveInspector().GetType();
+                        if (myMail.Sender == null) {
+                            myMail.BCC = myMail.Session.CurrentUser.AddressEntry.GetExchangeUser().PrimarySmtpAddress;
+                        }
+                    } catch (Exception) {
+                        ;
+                    }
+                })).Start();
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e) {
+            // Note: Outlook no longer raises this event. If you have code that 
+            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup() {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
